3a0f0d5e259267cc1e8df0e5c1d30ec2c61f9612
bolinkd <bolinkd@UVic.ca>
2015-09-21 15:29:30 -0700

    Completed Software License Checker

--- Validator.py
+++ Validator.py
@@ -1,52 +1,79 @@
-from Crypto.Cipher import AES
 from Crypto.PublicKey import RSA
-from Crypto.Hash import MD5
+from Crypto.Hash import SHA256
+from Crypto.Signature import PKCS1_v1_5
+from Crypto import Random
 import Crypto.Util.number
+import cPickle
 import argparse
 import datetime
 import os
+import sys
 
 parser = argparse.ArgumentParser(description='Software License Validator')
-parser.add_argument('location', metavar='S', type=str, nargs='?',
-                   help='Key Location')
-parser.add_argument('--generate', required=False, default=False, type=bool,
-                   help='Create A License Key')
-parser.add_argument('--day', required=False,
-				   help='Day Key Expires')
+parser.add_argument('--key', required=False, default=False, dest='key', action='store_true',
+                   help='Generate A Key')
+parser.add_argument('--generate', default=False, dest='generate', action='store_true',
+                   help='Create A License')
+parser.add_argument('--test', required=False, default=False, dest='test', action='store_true',
+                   help='Test a current License Key')
 args = parser.parse_args()
 
 location = os.getcwd()
 date = None
 
-key = RSA.generate(2048)
+if args.key == True:
+	print "Creating Key..."
+	rand = Random.new().read
+	key = RSA.generate(1024,rand)
+	with open("PrivateKey.PEM", 'wb') as s:
+		s.write(key.exportKey('PEM'))
 
-if args.location != None:
-	location = args.location
-if args.day == None:
+	with open("PublicKey.PEM", "wb") as s:
+		s.write(key.publickey().exportKey('PEM'))
+
+
+if args.generate == True:
+	print "Generating . . ."
+
 	date = datetime.datetime.now() + datetime.timedelta(days=31)
 	date = date.strftime("%d/%m/%Y")
-else:
-	date = datetime.datetime
-	date = date.strptime(args.day, "%d/%m/%Y")
-	date = date.strftime("%d/%m/%Y")
 
-if args.generate == False:
+	key = RSA.importKey(open('PrivateKey.PEM').read())
+	h = SHA256.new(date)
+	signer = PKCS1_v1_5.new(key)
+	signature = signer.sign(h)
 
-	f = open(location + "\Software License.txt", "r")
+	SoftwareLicense = open(location + "\Software License.txt", "w")
+	SoftwareLicense.write(date)
 
-	read = f.read()
-	temp = key.decrypt(read)
+	DigitalSignature = open(location + "\DigitalSignature.pkl", "wb")
+	cPickle.dump(signature, DigitalSignature, protocol=cPickle.HIGHEST_PROTOCOL)
 
-else:
+if args.test == True:
+	print "Testing . . ."
 
-	message = date
-	input_File = open(location + "\Date.txt", "r+")
-	input_File.write(message + '\n')
+	SoftwareLicense = open(location + "\Software License.txt", "r")
+	DigitalSignature = open(location + "\DigitalSignature.pkl", "rb")
 
-	hashMD5 = MD5.new()
-	hashMD5.update(message)
-	input_File.write(hashMD5.hexdigest())
+	SL = SoftwareLicense.read()
+	dateNow = datetime.datetime.now()
+	date = datetime.datetime.now()
+	date = date.strptime(SL, "%d/%m/%Y")
 
+	if date.year >= dateNow.year and  date.month >= dateNow.month and date.year >= dateNow.year:
+		print "Key Is Valid!"
+	else:
+		sys.exit("Key is Invalid")
+	
 
+	signature = cPickle.load(DigitalSignature)
 
-	output_File = open(location + "\Software License.txt", "w")
+	key = RSA.importKey(open('PublicKey.PEM').read())
+	h = SHA256.new(SL)
+	verifier = PKCS1_v1_5.new(key)
+	if verifier.verify(h, signature):
+	   print "Accessing Private Network"
+	   print "Download Complete!"
+	else:
+	    print "The Files Have Been Corrupted"
+
